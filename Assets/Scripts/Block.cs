3b57b6e6c8a4728232bb365fabc7e4d045f8042c
Lawrence Ling <Lawrence1995@hotmail.com>
2016-02-10 04:00:19 +1100

    Clicking a block now changes its colour

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class Block {
+    public enum Color {Clear, Blue, Green, Red, Yellow };
+
+    public static Sprite GetSprite(Color color) {
+        Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/sprite-sheet");
+        string[] spriteNames = new string[sprites.Length];
+
+        for (int i = 0; i < spriteNames.Length; i++) {
+            spriteNames[i] = sprites[i].name;
+        }
+
+        return sprites[Array.IndexOf(spriteNames, GetSpriteName(color))];
+    }
+
+    public static string GetSpriteName(Color color) {
+        switch (color) {
+            case Color.Clear:
+                return "clear-block";
+            case Color.Blue:
+                return "blue-block";
+            case Color.Green:
+                return "green-block";
+            case Color.Red:
+                return "red-block";
+            case Color.Yellow:
+                return "yellow-block";
+            default:
+                throw new Exception("Invalid block color");
+        }
+    }
+}
--- Assets/Scripts/BlockController.cs
+++ Assets/Scripts/BlockController.cs
@@ -5,11 +5,45 @@ public class BlockController : MonoBehaviour {
 
     public int speed;
 
-	void Start () {
-	
+    private SpriteRenderer spriteRenderer;
+    private Vector3 startPos;
+
+	void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startPos = transform.position;
 	}
-	
-	void FixedUpdate () {
+
+    void Update() {
+        Debug.Log(transform.position.y);
+        if (transform.position.y < -5.5) {
+            transform.position = startPos;
+        }
+
+        // On mouse down
+        if (Input.GetMouseButtonDown(0)) {
+            Block.Color blockColor;
+            switch (Random.Range(0, 4)) {
+                case 0:
+                    blockColor = Block.Color.Blue;
+                    break;
+                case 1:
+                    blockColor = Block.Color.Green;
+                    break;
+                case 2:
+                    blockColor = Block.Color.Red;
+                    break;
+                case 3:
+                    blockColor = Block.Color.Yellow;
+                    break;
+                default:
+                    throw new System.Exception("Invalid block color");
+            }
+
+            spriteRenderer.sprite = Block.GetSprite(blockColor);
+        }
+    }
+
+	void FixedUpdate() {
         Vector3 unit = new Vector3(0, -1, 0);
         transform.position += unit * speed * Time.deltaTime;
 	}

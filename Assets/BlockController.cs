d706ed740c75abd273279b4089c01b432b1561e4
Lawrence Ling <Lawrence1995@hotmail.com>
2016-02-09 15:32:43 +1100

    Block now moves

---
+++ Assets/BlockController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockController : MonoBehaviour {
+
+    public int speed;
+
+	void Start () {
+	
+	}
+	
+	void FixedUpdate () {
+        Vector3 unit = new Vector3(0, -1, 0);
+        transform.position += unit * speed * Time.deltaTime;
+	}
+}
